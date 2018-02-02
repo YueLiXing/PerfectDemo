15a59823221a4f2877aaa81182d6bf2de8a63b17
yuelixing <1223343444@qq.com>
2018-02-02 13:31:33 +0800

    添加 sql ，但是还。。。

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.3"),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", majorVersion: 3)
+        .package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", from: "3.0.1")
+//        .Package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", majorVersion: 3)
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
