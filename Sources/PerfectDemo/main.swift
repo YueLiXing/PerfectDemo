b1a1975796fa297d4f98c53c8d1ef4a410183a8e
yuelixing <1223343444@qq.com>
2018-02-02 16:25:37 +0800

    添加增加，查询接口

--- Sources/PerfectDemo/main.swift
+++ Sources/PerfectDemo/main.swift
@@ -4,13 +4,13 @@ import Foundation
 import PerfectSQLite
 import PerfectLib
 
-let dbPath = "./db/database"
+let dbPath = "../database.sqlite"
 do {
     let sqlite = try SQLite(dbPath, readOnly: false, busyTimeoutMillis: 10)
     defer {
         sqlite.close() // This makes sure we close our connection.
     }
-    try sqlite.execute(statement: "create table if not exists demo (uid integer primary key not null, name text not null)")
+    try sqlite.execute(statement: "create table if not exists demo (id integer primary key autoincrement,uid integer unique not null, name text not null)")
 } catch {
     Log.debug(message: "\(error)")
 }
@@ -19,15 +19,10 @@ var routes = Routes()
 routes.add(method: .get, uri: "/") {
     request, response in
     response.setHeader(.contentType, value: "text/html")
-    response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+    response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!<br>Haha,Kid!</body></html>")
         .completed()
 }
-routes.add(method: .get, uri: "/123") {
-    request, response in
-    response.setHeader(.contentType, value: "text/html")
-    response.appendBody(string: "<html><title></title><body>Hello, world!<br>123!</body></html>")
-        .completed()
-}
+
 routes.add(method: .get, uri: "/add") { (request, response) in
     let sqlite = try? SQLite(dbPath)
     defer {
@@ -38,8 +33,17 @@ routes.add(method: .get, uri: "/add") { (request, response) in
     let name = request.param(name: "name")
     var result = ""
     if uid != nil && name != nil {
-        result = "保存成功"
-        response.setBody(string: Tools.convertResult(data: result))
+        do {
+            try sqlite?.execute(statement: "insert into demo (uid,name) values(:1,:2)", doBindings: { (stmt) in
+                try? stmt.bind(position: 1, uid!)
+                try? stmt.bind(position: 2, name!)
+            })
+            result = "保存成功"
+            response.setBody(string: Tools.convertResult(data: result))
+        } catch {
+            response.setBody(string: Tools.convertResult(data: "\(error)"))
+            Log.error(message: "\(error)")
+        }
     } else {
         result = "参数错误"
         response.setBody(string: Tools.convertResult(code: 300, data: result))
@@ -53,14 +57,32 @@ routes.add(method: .get, uri: "/show") { (request, response) in
     defer {
         sqlite?.close() // This makes sure we close our connection.
     }
-    
-    try? sqlite?.forEachRow(statement: "select * from demo", handleRow: { (stmt, index) in
-        dictArray.append([
-            "uid": stmt.columnInt(position: 0),
-            "name": stmt.columnText(position: 1)
-            ])
-    })
-    response.setBody(string:Tools.convertResult(data: dictArray))
+    let uid = request.param(name: "uid")
+    if uid == nil {
+        try? sqlite?.forEachRow(statement: "select * from demo", handleRow: { (stmt, index) in
+            Log.debug(message: "\(stmt)");
+            dictArray.append([
+                "uid": stmt.columnInt(position: 1),
+                "name": stmt.columnText(position: 2)
+                ])
+        })
+        Log.debug(message: dictArray.description)
+        response.setBody(string:Tools.convertResult(data: dictArray))
+    } else {
+        try? sqlite?.forEachRow(statement: "select * from demo where uid = "+uid!, handleRow: { (stmt, index) in
+            Log.debug(message: "\(stmt)");
+            dictArray.append([
+                "uid": stmt.columnInt(position: 1),
+                "name": stmt.columnText(position: 2)
+                ])
+        })
+        Log.debug(message: dictArray.description)
+        if dictArray.count > 0 {
+            response.setBody(string:Tools.convertResult(data: dictArray))
+        } else {
+            response.setBody(string:Tools.convertResult(data: "没有对应的数据"))
+        }
+    }
     response.completed()
 }
 
@@ -72,4 +94,11 @@ do {
 } catch {
     fatalError("\(error)") // fatal error launching one of the servers
 }
-
+/*
+ http://0.0.0.0:8181/show
+ http://0.0.0.0:8181/add?uid=1&name=%E7%BB%A7%E5%88%9A
+ http://0.0.0.0:8181/add?uid=2&name=%E5%B0%8F%E7%BA%A2
+ http://0.0.0.0:8181/add?uid=3&name=lala
+ http://0.0.0.0:8181/show
+ http://0.0.0.0:8181/show?uid=3
+ */
